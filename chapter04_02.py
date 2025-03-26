f820330d8800a686e76ee0cab594b894c67b5299
SongSunJoo <mundalling@naver.com>
2025-03-26 14:26:49 +0900

    if문, for문

---
+++ chapter04_02.py
@@ -0,0 +1,15 @@
+# Chapter04-2
+# 파이썬 반복문
+# FOR 실습
+
+# 코딩의 실습
+# for in <collection>
+#   <Loop body>
+
+for v1 in range(10):    # 0 ~ 9
+    print('v1 is : ', v1)
+
+print()
+
+for v2 in range(1, 10): # 1 ~ 10
+    print('v2 is : ', v2)
